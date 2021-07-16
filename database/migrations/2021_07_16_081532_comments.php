df7eb7e10f806bb26c11ed6144fa806be3924e91
HUST\long.pt173247 <longgtc123@gmail.com>
2021-07-16 17:23:25 +0700

    Blog

---
+++ database/migrations/2021_07_16_081532_comments.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Comments extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->string('id_user');
+            $table->string('id_post');
+            $table->string('content');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
